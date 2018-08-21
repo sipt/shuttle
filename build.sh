50605c99e1ec1b89c8f216d75895f6d94ef84d57
sipt <1182102908@qq.com>
2018-08-21 19:13:11 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/env bash
 
+# ./build.sh mac|win|linux
+
 if [ ! -d "_output/" ];then
 mkdir _output
+fi
+if [ ! -d "_output/shuttle" ];then
+mkdir _output/shuttle
 else
-rm -rf _output/*
+rm -rf _output/shuttle/*
 fi
 
-cp -rf view _output/
-cp GeoLite2-Country.mmdb _output/
-if [ -d ".conf/" ];then
-cp .conf/sipt.yaml _output/shuttle.yaml
-else
-cp example.yaml _output/shuttle.yaml
+cp -rf view _output/shuttle/
+cp GeoLite2-Country.mmdb _output/shuttle/
+#if [ -d ".conf/" ];then
+#cp .conf/sipt.yaml _output/shuttle.yaml
+#else
+cp example.yaml _output/shuttle/shuttle.yaml
+#fi
+
+if [ "$1" == "mac" ];then
+# mac os
+GOOS=darwin GOARCH=amd64 go build -o _output/shuttle/shuttle cmd/main.go
+`echo "nohup ./shuttle >> shuttle.log 2>&1 &" > _output/shuttle/start.sh`
+`chmod a+x _output/shuttle/start.sh`
+elif [ "$1" == "win" ];then
+# windows
+GOOS=windows GOARCH=amd64 go build -o _output/shuttle/shuttle.exe cmd/main.go
+`echo "@echo off
+if \"%1\" == \"h\" goto begin
+mshta vbscript:createobject(\"wscript.shell\").run(\"%~nx0 h\",0)(window.close)&&exit
+:begin
+shuttle >> shuttle.log" > _output/shuttle/startup.bat`
+elif [ "$1" == "linux" ];then
+# linux
+GOOS=linux GOARCH=amd64 go build -o _output/shuttle/shuttle cmd/main.go
+`echo "nohup ./shuttle >> shuttle.log 2>&1 &" > _output/shuttle/start.sh`
+`chmod a+x _output/shuttle/start.sh`
 fi
-go build -o _output/shuttle cmd/main.go
-
-echo "nohup ./shuttle >> shuttle.log 2>&1 &" > _output/start.sh
-chmod a+x _output/start.sh
-
-
