d7c7cbbd737abc2baa3850054a5d4cfdd95a704b
sipt <1182102908@qq.com>
2018-09-27 02:58:46 +0800

    fix version

--- config.go
+++ config.go
@@ -1,13 +1,13 @@
 package shuttle
 
 import (
-	"net"
-	"io/ioutil"
 	"fmt"
-	"github.com/sipt/yaml"
 	"github.com/sipt/shuttle/log"
-	"strings"
+	"github.com/sipt/yaml"
+	"io/ioutil"
+	"net"
 	"regexp"
+	"strings"
 )
 
 const ControllerDomain = "c.sipt.top"
@@ -391,4 +391,4 @@ func as_hex(b []byte, i int) int {
 	return int(bi) - '0'
 }
 
-var ShuttleVersion = "v0.3.0"
+var ShuttleVersion = "v0.5.0"
