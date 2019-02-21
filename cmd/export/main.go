3094909fb2b808ad1c6249fcc75b43dfefba8c65
sipt <1182102908@qq.com>
2019-02-22 00:29:58 +0800

    fix export

--- cmd/export/main.go
+++ cmd/export/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "github.com/sipt/shuttle/cmd"
+import (
+	"encoding/json"
+	"github.com/sipt/shuttle/cmd"
+)
+import "C"
+
+type Config struct {
+	LogMode    string `json:"log_mode"`
+	LogPath    string `json:"log_path"`
+	ConfigPath string `json:"config_path"`
+}
 
 //export Run
-func Run(logMode, logPath, configPath string) {
-	go cmd.Run(logMode, logPath, configPath)
+func Run(confStr string) int {
+	conf := &Config{}
+	err := json.Unmarshal([]byte(confStr), conf)
+	if err != nil {
+		return 1
+	}
+	go cmd.Run(conf.LogMode, conf.LogPath, conf.ConfigPath)
+	return 0
 }
 
 func main() {
